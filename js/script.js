3b11ff0279991045b635160d614ab1c609d7ba97
Somila Tshapu <82814273+SomilaTshapu1@users.noreply.github.com>
2022-07-21 16:09:21 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const navbarTogglerBtn = document.querySelector('.navbar-toggler');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+
+navbarTogglerBtn.addEventListener('click', () =>{
+    navbarCollapseDiv.classList.toggle('show-navbar-collapse');
+    if(navbarCollapseDiv.classList.contains('show-navbar-collapse')){
+        navbarTogglerBtn.querySelector('img').src = "close.png"
+    }
+    else(navbarTogglerBtn.querySelector('img').src = "images/menu.png")
+})
+//fixed margin nav bar
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () =>{
+    if(window.pageYOffset > 100){
+        header.classList.add('fixed-navbar');
+    }
+    else{
+        header.classList.remove('fixed-navbar');
+    }
+})
